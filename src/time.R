a62a3c198e9e18f3e51ddec6a95e84e3937c9660
Scott Briggs <hopingforclearskies@gmail.com>
2021-04-15 22:39:32 -0500

    Updated tests for time conversions

---
+++ src/time.R
@@ -0,0 +1,19 @@
+
+# Convert decimal hours to hours, minutes, and seconds
+HHMMSS <- function (HHdd)
+{
+    intpart1 <- trunc(HHdd)
+    fracpart1 <- HHdd - intpart1
+    hh = intpart1
+    temp = fracpart1 * 60
+    mm <- trunc(temp)
+    ss = trunc((temp - mm) * 60)
+    
+    c(hh, mm, ss)
+}
+
+# Converts hours, minutes, and seconds to decimal hours
+HHdd <- function (hr, min, sec)
+{
+    decimal_hours <- hr + min  /60 + sec / 3600
+}
