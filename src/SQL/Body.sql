939cd03a8c7dc2638b44a85dc19916fa5eadb9a8
Scott Briggs <hopingforclearskies@gmail.com>
2021-04-27 22:00:49 -0500

    Initial commit of DDL for database tables

---
+++ src/SQL/Body.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Body (
+    Body TEXT NOT NULL
+  , Units TEXT NOT NULL
+  , Center TEXT NOT NULL
+  , Properties INTEGER NOT NULL
+  , Start Offset INTEGER NOT NULL
+  , Coefficients INTEGER NOT NULL
+  , Subintervals INTEGER NOT NULL
+);
